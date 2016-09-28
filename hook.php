78ad50d4d8e40edddcc306557ab387c7e11b01dc
luunguyen <luunguyen@ccintegration.com>
2016-09-28 17:55:31 +0700

    Update

--- hook.php
+++ hook.php
@@ -1,14 +1,12 @@
 <?php
 // set autoload
-//spl_autoload_register(function ($class) {
-//    require_once(str_replace('\\', '/', $class . '.php'));
-//});
+spl_autoload_register(function ($class) {
+    require_once(str_replace('\\', '/', $class . '.php'));
+});
 
-use Project\Hook\Client as Client;
+use Project\Hook\Client;
 
-$hook = new Client()
-
-$hook::configure(array(
+$hook = Client::configure(array(
   'app_id' => 1,
   'key' => '638d9ba9643e568c1065084e3e67fb7e',
   'endpoint' => 'http://172.16.4.56:4665/index.php/'
